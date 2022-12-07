9ef474802e13b9f3ca6457b45173aa52c97e446e
Pete Campbell <94608066+lowlevelcodehustler@users.noreply.github.com>
2022-12-07 14:41:05 +0000

    Update wafv.js

--- functions/wafv.js
+++ functions/wafv.js
@@ -2,3 +2,8 @@ var imageUrls = [
  './static/250x250px_grey.png',
  './static/250x250px_black.png'
 ];
+
+var img = document.getElementById('image');
+img.addEventListener("click", function() {
+    this.src = imageUrls[Math.floor(Math.random() * imageUrls.length)];
+});
