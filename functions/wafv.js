3cb8619e9ad0a4216b8a50ed8ff1870831f8888c
Pete Campbell <94608066+lowlevelcodehustler@users.noreply.github.com>
2022-12-08 13:36:29 +0000

    Update wafv.js

--- functions/wafv.js
+++ functions/wafv.js
@@ -1,15 +1,14 @@
 var imageUrls = [
- './static/250x250px_grey.png',
- './static/250x250px_black.png'
+ "./static/250x250px_grey.png",
+ "./static/250x250px_black.png"
 ];
 
-var img = document.getElementById('flipatrandom');
-var time = 1200000;
+var img = document.getElementById("flipatrandom");
 
 setInterval(wafv, 1200000);
 
 function wafv() {
  img.addEventListener("click", function() {
      this.src = imageUrls[Math.floor(Math.random() * imageUrls.length)];
-})
- });
+});
+ }
